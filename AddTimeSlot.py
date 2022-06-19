77398512a3962abe724b2b9f4ce22d8c4c82b1c7
xerone9 <48822352+xerone9@users.noreply.github.com>
2022-06-19 12:07:27 +0500

    Final For Publish
    Removed Unnecessary Files

--- AddTimeSlot.py
+++
@@ -1,4 +0,0 @@
-list=["haider", "haider", "umer", "usman", "usman"]
-list.remove("haider")
-list.remove("usman")
-print(list)
--- adminuser.py
+++
@@ -1,7 +0,0 @@
-from SQLWorking import *
-
-date = "10/04/2022"
-game = fetch_values_from_time_table(date)
-
-for value in game:
-    print(value)
